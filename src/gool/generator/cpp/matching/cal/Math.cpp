8e17fd78a8e7f3d2845112b170c80a7a7469d58c
johan <johan@asusX201.lan>
2014-01-08 17:13:42 +0100

    Librairy Math, cpp + java

---
+++ src/gool/generator/cpp/matching/cal/Math.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include "Math.h"
+
+#include "stdio.h"
+#include "fstream"
+#include "cstdio"
+
+
+
+
+	double Math::exp(double n){
+		return exp(n);
+	}
+
+	double Math:log(double n){
+		return log(n);
+	}
+
+	double Math::pow(double n1,double n2){
+		return pow(n1,n2);
+	}
+
+
---
+++ src/gool/generator/cpp/matching/cal/Math.h
@@ -0,0 +1,23 @@
+#include<iostream>
+#ifndef __GOOLMATHIMPL_H
+#define __GOOLMATHIMPL_H
+
+
+#include "stdio.h"
+#include "fstream"
+#include "cstdio"
+#include "math.h"
+
+
+
+
+class Math  {
+
+	static public: 	double exp(double n);
+	static public: 	double log(double n);
+	static public: 	double pow(double n1, double n2);
+
+
+	
+};
+#endif
