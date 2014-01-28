c466bc4be3eb177d48a6c85b60c546a873af6761
Don <donatien@Ubuntu-Don>
2014-01-28 11:44:04 +0100

    Fichiers tests

---
+++ GOOLINPUTCPP/SimpleIf.cpp
@@ -0,0 +1,8 @@
+int main() {
+int b = 1;
+if (b) {
+	cout << (2 + 2) << endl;
+} else {
+	cout << (2 + 5) << endl;
+}
+}
---
+++ GOOLINPUTCPP/SimpleNew.cpp
@@ -0,0 +1,18 @@
+int ajouteDeux(int nombreRecu)
+{
+    int valeur=nombreRecu + 2;
+
+    return valeur;
+}
+
+int main()
+{
+    int a=2,b=2;
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+    b = ajouteDeux(a); //Appel de la fonction
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+
+    return 0;
+}
