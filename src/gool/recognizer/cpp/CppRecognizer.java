5c231df1089e4891fd664fc680aa618f811ecf6b
pierre <pierre@pierre-K53SV>
2014-01-30 09:27:23 +0100

    proper

--- src/gool/recognizer/cpp/CppRecognizer.java
+++ src/gool/recognizer/cpp/CppRecognizer.java
@@ -66,10 +66,8 @@ import gool.recognizer.common.GoolLibraryClassAstBuilder;
 import gool.recognizer.common.RecognizerMatcher;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
